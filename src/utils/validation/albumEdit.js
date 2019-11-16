91ff554a9e57a5c175f75eadb5c2f741ada87c92
Nithin Madhuranga <nithin@axis.lk>
2019-11-16 12:35:18 +0800

    Update validation for URL

--- src/utils/validation/albumEdit.js
+++ src/utils/validation/albumEdit.js
@@ -1,12 +1,23 @@
+import validator from 'validator'
+
 export default function(values) {
     const errors = {};
     const requiredFields = [
-        'firstName'
+        'title',
+        'url'
     ];
     requiredFields.forEach(field => {
         if (!values[field]) {
             errors[field] = 'Required';
         }
     });
+
+    if(typeof values.url !== "undefined") {
+        if (!validator.isURL(values.url)) {
+            errors.url = 'This field can not be link'
+        }
+    }
+
+
     return errors;
 }
